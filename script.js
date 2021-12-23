addbff16af34735c82b5c88f5f23087181d95a51
irina-maleeva <iwmaleeva@gmail.com>
2021-12-23 21:01:22 +0300

    Feature1_create-todo adding functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+let sortButton = document.querySelector('.sort');
+let addButton = document.querySelector('.add');
+let removeButton = document.querySelector('.remove');
+let input = document.querySelector('input')
+
+input.addEventListener('keyup', (event) => {
+    if (event.code == 'Enter') {
+    <li></li>
+    }
+});
