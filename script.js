68894357df79b683fd2e546f09f8b3b27d233477
irina-maleeva <iwmaleeva@gmail.com>
2021-12-25 14:05:22 +0300

    buttons add and remove are active, now working on sorting butto

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 let sortButton = document.querySelector('.sort');
 let addButton = document.querySelector('.add');
-let removeButton = document.querySelector('.remove');
-let input = document.querySelector('input')
+let input = document.querySelector('input');
+let ul = document.querySelector('ul');
+let sortReversedButton = document.querySelector('.sort');
 
-input.addEventListener('keyup', (event) => {
-    if (event.code == 'Enter') {
-    <li></li>
-    }
+// добавление строчки списка с кнопкой удаления, и функционал кнопки удаления
+addButton.addEventListener('click', () => {
+       let li = document.createElement('li');
+        li.innerHTML = 
+        `<li> ${input.value} <p><img class='remove' src=./remove_button.png alt="кнопка удаления"></p></li>`;
+        li.querySelector('img').addEventListener('click', (event) => {
+            event.target.parentElement.parentElement.remove();   
+            });
+        ul.append(li);
+        input.value = '';      
 });
+
+
+    // сортировка по алфавиту - пока не работает, ищу ошибку
+sortButton.addEventListener('click', () => {
+
+    let listArray = Array.from(document.querySelectorAll('li'));
+    listArray.sort((a, b) => {
+    return a.firstElementChild.innerHTML.localeCompare(b.firstElementChild.innerHTML);
+    })
+    let li = document.querySelector('li');
+    ul.innerHTML = null;
+    listArray.forEach(element => {
+        ul.append(element);
+    })
+});
+
+  // сортировка в обратном порядке - пока не до конца прописана
+// sortReversedButton.addEventListener('click', () => {
+//   
+//     listArray.sort((a, b) => {
+//     return a.firstElementChild.innerHTML.localeCompare(b.firstElementChild.innerHTML)*-1;
+// //     console.log(rowArray);
+//     })
+// });
+
+
