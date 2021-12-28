8a36b8d14bc5658509b140ce3e8951427649f052
irina-maleeva <iwmaleeva@gmail.com>
2021-12-28 14:37:21 +0300

    Sorting button is working and changing color. Hover effects working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let sortButton = document.querySelector('.sort');
 let addButton = document.querySelector('.add');
 let input = document.querySelector('input');
 let ul = document.querySelector('ul');
-let sortReversedButton = document.querySelector('.sort');
+let sortImage = document.querySelector('.sort');
 
 // добавление строчки списка с кнопкой удаления, и функционал кнопки удаления
 addButton.addEventListener('click', () => {
@@ -17,27 +17,28 @@ addButton.addEventListener('click', () => {
 });
 
 
-    // сортировка по алфавиту - пока не работает, ищу ошибку
-sortButton.addEventListener('click', () => {
+// сортировка в обе стороны с добавлением переменной оrder, которая меняется
+let order = 1;
 
+sortButton.addEventListener('click', () => {
     let listArray = Array.from(document.querySelectorAll('li'));
     listArray.sort((a, b) => {
-    return a.firstElementChild.innerHTML.localeCompare(b.firstElementChild.innerHTML);
-    })
-    let li = document.querySelector('li');
+        return a.innerText.localeCompare(b.innerText)*(order); 
+    })             
     ul.innerHTML = null;
     listArray.forEach(element => {
         ul.append(element);
     })
+    order = order*(-1);
+    switch(order) {
+        case 1:
+        sortImage.src = './sort_down.png';
+        break;
+        case -1:
+        sortImage.src = './sort_up.png';
+        break;
+    }
+    
 });
-
-  // сортировка в обратном порядке - пока не до конца прописана
-// sortReversedButton.addEventListener('click', () => {
-//   
-//     listArray.sort((a, b) => {
-//     return a.firstElementChild.innerHTML.localeCompare(b.firstElementChild.innerHTML)*-1;
-// //     console.log(rowArray);
-//     })
-// });
 
 
