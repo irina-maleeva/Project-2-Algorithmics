d3646cedd374c3ed8cfd55d5d26e752dc8b56579
irina-maleeva <iwmaleeva@gmail.com>
2021-12-29 18:19:39 +0300

    added drag and drop - its working but with 2 mistakes (see comments in js file

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ addButton.addEventListener('click', () => {
         li.querySelector('img').addEventListener('click', (event) => {
             event.target.parentElement.parentElement.remove();   
             });
+    // добавила тут аттрибут draggable, чтобы можно было потом перемещать
+        li.draggable = true;
+
         ul.append(li);
         input.value = '';      
 });
@@ -41,4 +44,26 @@ sortButton.addEventListener('click', () => {
     
 });
 
+
+// Добавляю drag and drop к элементам списка
+// аттрибут draggable добавила выше при создании записи li
+// работает, но c двумя ошибками:  
+// 1) работает только пока не нажмешь кнопку сортировки, после нажатия кнопки сортировки уже не работает; 
+// 2) перетаскивет как саму запись списка, так и картинку внутри списка - может перетащить картинку удаления без самого текста
+ul.addEventListener('dragstart', (event) => {
+    event.target.classList.add('selected');
+})
+ul.addEventListener('dragend', (event) => {
+    event.target.classList.remove('selected');
+});
+
+ul.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    let activeLi = document.querySelector('.selected');
+    let currentLi = event.target;
+    // if (!activeLi.contains('li')) {
+    //     return;
+    // }
+    ul.insertBefore(activeLi, currentLi.nextElementSibling);
+});
 
